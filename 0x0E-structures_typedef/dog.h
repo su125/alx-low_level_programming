f0827e2901fccc836fde0beb416873e8d9fa40d4
su125 <souhaila.boussaid2@gmail.com>
2023-04-17 15:21:33 -0700

    dog.h updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,8 +15,8 @@ char *owner;
 /**
  * dog_t - typedef for struct dog
  */
-typedef strucr dog dog_t;
-void init_dog(struct dog*d, char *name, float age, char *owner);
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_gdog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
