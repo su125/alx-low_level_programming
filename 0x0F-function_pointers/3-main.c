20b0177941cec336fc832403c547130ef4c0cf18
su125 <souhaila.boussaid2@gmail.com>
2023-04-19 17:50:12 -0700

    task 3 calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,13 +8,13 @@
  * @argv: an array of pointers to the arguments
  * Return: 0 always
  */
-int main(int __attribute__((__unsed__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 int num1, num2;
 char *op;
 if (argc != 4)
 {
-printf("ERROR\n")
+printf("ERROR\n");
 exit(98);
 }
 num1 = atoi(argv[1]);
@@ -30,6 +30,6 @@ if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 printf("ERROR\n");
 exit(100);
 }
-printd("%d\n", get_op_func(op)(num1, num2));
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
