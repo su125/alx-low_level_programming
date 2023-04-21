90dc99149876822c2a604d75f3233cd6a658e9cc
su125 <souhaila.boussaid2@gmail.com>
2023-04-21 16:34:51 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef _VARIADIC__H
+#define _VARIADIC_H
+int _putchar(char c);
+int sum_them_al(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
