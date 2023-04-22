043eb17d9aa4b4f435418a6a41eb3e5a781c0628
su125 <souhaila.boussaid2@gmail.com>
2023-04-21 17:54:31 -0700

    updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
-#ifndef _VARIADIC__H
-#define _VARIADIC_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_ 
 int _putchar(char c);
-int sum_them_al(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
