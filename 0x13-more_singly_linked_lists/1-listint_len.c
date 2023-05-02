5c826126e83758a5ba4eed8838830f606dfcd0cc
su125 <souhaila.boussaid2@gmail.com>
2023-05-02 08:09:04 -0700

    task 1 listint len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,8 +2,7 @@
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
- * Description: return num of elem in linked list 
- * return: number of nodes
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,4 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
