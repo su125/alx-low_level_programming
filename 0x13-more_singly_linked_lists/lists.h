0818206dfd400bd0ad94804f34980937558742e7
su125 <souhaila.boussaid2@gmail.com>
2023-05-03 14:38:23 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,5 +24,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 size_t listint_len(const listint_t *h);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 listint_t *reverse_listint(listint_t **head);
 #endif
