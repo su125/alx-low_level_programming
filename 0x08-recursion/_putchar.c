7f42b6c4f2003b394bacadfb6d82e8db3d50d2fb
su125 <souhaila.boussaid2@gmail.com>
2023-06-07 09:07:14 -0700

    task 0 : puts_recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the chatacter to print
+ * Return: on success 1, on error -1
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
