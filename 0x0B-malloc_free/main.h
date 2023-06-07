d92e2e0e6c2f48066bfca7f76f29f131675e239f
souhaila <46802542+su125@users.noreply.github.com>
2023-06-07 15:17:02 +0100

    Delete 0x0B-malloc_free directory

--- 0x0B-malloc_free/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-char *create_array(unsigned int size, char c );
-int _putchar(char c);
-char *_strconcat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void fre_frid(int **grid, int height);
-char *argstostr(int ac, char **av);
-#endif
