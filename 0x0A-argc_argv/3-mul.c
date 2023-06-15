bec66ea4c394abae4121c4a05c1e947967c729c9
su125 <souhaila.boussaid2@gmail.com>
2023-06-14 17:32:09 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -37,3 +37,23 @@ if (f == 0)
 return (0);
 return (n);
 }
+/**
+ * main - multiplies two num
+ * @argc: num of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+int result, num1, num2;
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
+}
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
+result = num1 *num2;
+printf("%d\n", result);
+return (0);
+}
