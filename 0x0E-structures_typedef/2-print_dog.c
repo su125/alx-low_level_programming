288493f791cd2301e1510ed4e1f1cfe02e5633c2
su125 <souhaila.boussaid2@gmail.com>
2023-06-20 16:14:05 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,5 +13,5 @@ if (d->name == NULL)
 d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
-printf ("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
