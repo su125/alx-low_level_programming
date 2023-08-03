0492da4823caafc99377516ac6e700d9a37fa302
su125 <souhaila.boussaid2@gmail.com>
2023-08-03 15:56:30 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recurion - returns the length of a sring
+ * @s: the string to measured
+ * Return: the length o the string
+ */
+int _strlen_recursion(char *s)
+{
+int longit = 0;
+if (*s)
+{
+longit++;
+longit+= _strlen_recursion(s + 1);
+}
+return (longit);
+}
