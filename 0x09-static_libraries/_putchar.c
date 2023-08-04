4e752d8e3fdf4b82a99eb14475dd4b41b70e6f8f
su125 <souhaila.boussaid2@gmail.com>
2023-08-04 03:02:26 -0700

    putchar

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes a character c the stdout
+ * @c: the character to print
+ * Return: 1 on succes, -1 otherwise
+ */
+int _putchar(char *c)
+{
+return (1, &c, 1);
+}
