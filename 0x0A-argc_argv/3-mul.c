ae759fa9d2ab291c45e3157e4193c4456525d177
su125 <souhaila.boussaid2@gmail.com>
2023-08-09 17:44:55 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,14 +7,14 @@
  */
 int _atoi(char *s)
 {
-int i, d, n, len, digit;
+int i, d, n, len, f,  digit;
 i = 0;
 d = 0;
 n = 0;
 len = 0;
 f = 0;
 digit = 0;
-while (s[len] != '\0')
+while (s[len] !='\0')
 len++;
 while (i < len && f == 0)
 {
@@ -25,7 +25,7 @@ if (s[i] > '0' && s[i] <= '9')
 digit = s[i] - '0';
 if (d % 2)
 digit = -digit;
-n = n  10 + digit;
+n = n  * 10 + digit;
 f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
@@ -45,8 +45,8 @@ return (n);
  */
 int main(int argc, char *argv[])
 {
-int result, num1, num 2;
-if (argc < 3 || argc > 3)
+int result, num1, num2;
+if(argc < 3 || argc > 3)
 {
 printf("ERROR\n");
 return (1);
